9846b4693675270e26e952cac6731164aeb12de7
RB <redabey82@gmail.com>
2023-05-07 17:57:57 +0200

    Commit.

--- caesar_cipher/rust/caesar_cipher/src/cipher/mod.rs
+++ caesar_cipher/rust/caesar_cipher/src/cipher/mod.rs
@@ -1,3 +1,7 @@
 pub fn caesar_encrypt(i: u8, k: u8) -> u8 {
     (i + (k % 26)) % 26
 }
+
+pub fn caesar_decrypt(i: u8, k: u8) -> u8 {
+    (i - (k % 26)) % 26
+}
--- caesar_cipher/rust/caesar_cipher/src/test/cipher_tests.rs
+++ caesar_cipher/rust/caesar_cipher/src/test/cipher_tests.rs
@@ -1,5 +1,5 @@
 #[cfg(test)]
-use crate::cipher::caesar_encrypt;
+use crate::cipher::{caesar_encrypt, caesar_decrypt};
 
 #[test]
 fn test_caesar_encrypt() {
@@ -9,4 +9,14 @@ fn test_caesar_encrypt() {
     assert_eq!(caesar_encrypt(0, 25), 25);
     assert_eq!(caesar_encrypt(10, 16), 0);
     assert_eq!(caesar_encrypt(25, 1), 0);
+}
+
+#[test]
+fn test_caesar_decrypt() {
+    assert_eq!(caesar_decrypt(0, 0), 0);
+    assert_eq!(caesar_decrypt(0, 26), 0);
+    assert_eq!(caesar_decrypt(0, 52), 0);
+    assert_eq!(caesar_decrypt(25, 25), 0);
+    // assert_eq!(caesar_decrypt(0, 16), 10);
+    // assert_eq!(caesar_decrypt(0, 1), 25);
 }
