52c9628bf5eb78d1248c57fe337fd26ccf2d844b
rb <redabey82@gmail.com>
2023-05-09 15:57:49 +0200

    Commit.

--- caesar_cipher/rust/caesar_cipher/src/test/cipher_tests.rs
+++ caesar_cipher/rust/caesar_cipher/src/test/cipher_tests.rs
@@ -1,22 +1,19 @@
 #[cfg(test)]
-use crate::cipher::{caesar_encrypt, caesar_decrypt};
+use crate::cipher::{caesar_decrypt, caesar_encrypt};
 
 #[test]
 fn test_caesar_encrypt() {
-    assert_eq!(caesar_encrypt(0, 0), 0);
-    assert_eq!(caesar_encrypt(0, 26), 0);
-    assert_eq!(caesar_encrypt(0, 52), 0);
-    assert_eq!(caesar_encrypt(0, 25), 25);
-    assert_eq!(caesar_encrypt(10, 16), 0);
-    assert_eq!(caesar_encrypt(25, 1), 0);
+    assert_eq!(caesar_encrypt(0, 0, 26), Some(0));
+    assert_eq!(caesar_encrypt(0, 26, 26), Some(0));
+    assert_eq!(caesar_encrypt(0, 52, 26), Some(0));
+    assert_eq!(caesar_encrypt(0, 25, 26), Some(25));
+    assert_eq!(caesar_encrypt(10, 16, 26), Some(0));
+    assert_eq!(caesar_encrypt(25, 1, 26), Some(0));
 }
 
 #[test]
 fn test_caesar_decrypt() {
-    assert_eq!(caesar_decrypt(0, 0), 0);
-    assert_eq!(caesar_decrypt(0, 26), 0);
-    assert_eq!(caesar_decrypt(0, 52), 0);
-    assert_eq!(caesar_decrypt(25, 25), 0);
-    // assert_eq!(caesar_decrypt(0, 16), 10);
-    // assert_eq!(caesar_decrypt(0, 1), 25);
+    assert_eq!(caesar_decrypt(0, 0, 26), Some(0));
+    assert_eq!(caesar_decrypt(0, 26, 26), Some(0));
+    assert_eq!(caesar_decrypt(25, 25, 26), Some(0));
 }
